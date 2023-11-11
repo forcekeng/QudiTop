5aaa44eabbdf28c0856f722f2874a95f55cf9b5d
Force Keng <forcekeng@126.com>
2023-11-11 14:08:12 +0800

    add __init__

---
+++ quditop/__init__.py
@@ -0,0 +1,5 @@
+"""The provided interfaces."""
+
+from circuit import Circuit
+from gates import X, Y, Z, H, RX, RY, RZ, GH, INC, SWAP, MVCG, UMG
+from expectation import Expectation
--- quditop/expectation.py
+++ quditop/expectation.py
@@ -7,8 +7,9 @@ from quditop.gates import GateBase
 
 
 class Expectation(nn.Module):
-    """
-    ham = [(1.0, Z(1,2,3).on(1)), (2.0, X(1, 0, 3).on(2))]
+    """Get the expectation with given hamiltonian and quantum state.
+    Hamiltonian can be written as a list, each element(a tuple) is composed of coefficient and a none parameter gate.
+    e.g. ham = [(1.0, Z(1,2,3).on(1)), (2.0, X(1, 0, 3).on(2))]
     """
 
     def __init__(self, ham: List):
